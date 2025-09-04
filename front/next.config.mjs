a5164b456d0aeb27ce8612215ea5d18f0ffd120b
Malek Kadri <93520204+malekkadri@users.noreply.github.com>
2025-09-04 17:52:36 +0100

    Use medias table for album media

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -8,6 +8,20 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/**",
+      },
+    ],
   },
 }
 
